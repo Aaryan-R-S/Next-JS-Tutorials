feabaf765d84769a71dd1cb6a79a551792ff839b
Aaryan-R-S <aaryan20004@iiitd.ac.in>
2022-02-10 19:12:49 +0530

    Done till Vid 7 (Hunitng Coder CSS Editing)

--- Tutorials/huntingcoder/pages/index.js
+++ Tutorials/huntingcoder/pages/index.js
@@ -20,6 +20,15 @@ export default function Home() {
       {/* To run or inject custom javascript in the application [Inbuilt next js feature to specify when to run the script- "beforeInteractive", "afterInteractive", or "lazyOnload"] */}
       {/* <Script src="/my_script.js" strategy="lazyOnload"></Script> */}
 
+      <nav className={styles.mainnav}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+
       <main className={styles.main}>
         <h1 className={styles.title}>
           Hunting Coder
@@ -28,50 +37,24 @@ export default function Home() {
         <p className={styles.description}>
           A blog for hunting coders by a hunting coder!
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        
+        <div className="blogs">
+          <h2>Popular Blogs</h2>
+          <div className="blogItem">
+            <h3>How to learn JavaScript in 2022?</h3>
+            <p>JavaScritp is the language used to design logic for the web</p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn JavaScript in 2022?</h3>
+            <p>JavaScritp is the language used to design logic for the web</p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn JavaScript in 2022?</h3>
+            <p>JavaScritp is the language used to design logic for the web</p>
+          </div>
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
