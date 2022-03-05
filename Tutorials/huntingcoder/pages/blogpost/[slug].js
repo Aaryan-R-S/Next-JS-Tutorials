dc5c0d265fcfba5827054c3b2aa49cd653dc9832
Aaryan-R-S <aaryan20004@iiitd.ac.in>
2022-03-05 18:44:47 +0530

    Done till Vid 22 Server side rendering

--- Tutorials/huntingcoder/pages/blogpost/[slug].js
+++ Tutorials/huntingcoder/pages/blogpost/[slug].js
@@ -1,23 +1,12 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import {useRouter} from 'next/router'
 import styles from '../../styles/BlogPost.module.css'
 
 // Step 1: Find the file corresponding to the slug
 // Step 2: Populate them inside the page
 
-export default function Slug() {
-  const router = useRouter();
-  
-  const [blog, setBlog] = useState([])
-  useEffect(() => {
-    if(!router.isReady){return;}
-    const {slug} = router.query;
-    fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a)=>{
-      return a.json();
-    }).then((parsed)=>{
-        setBlog(parsed)
-      })
-  }, [router.isReady])
+export default function Slug(props) {
+  const [blog, setBlog] = useState(props.myBlog)
   
   return (
     blog && <div className={styles.container}>
@@ -28,4 +17,13 @@ export default function Slug() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const {slug} = context.query;
+  let data = await fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+  let myBlog = await data.json();
+  return{
+    props:{myBlog},
+  }
 }
