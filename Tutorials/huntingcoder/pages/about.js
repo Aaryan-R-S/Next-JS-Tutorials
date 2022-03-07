c2836ef6b7d462637c06e2b4181c5350bf524823
Aaryan-R-S <aaryan20004@iiitd.ac.in>
2022-03-07 16:44:51 +0530

    Added till vid 27 About & Contact Page

--- Tutorials/huntingcoder/pages/about.js
+++ Tutorials/huntingcoder/pages/about.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import styles from "../styles/About.module.css";
 
 export default function About() {
   return (
-    <div>This is my about page.</div>
+    <div className={styles.container}>
+      <h1 className={styles.center}>About Hunting Coder</h1>
+      <h2>Introduction</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod ab cum animi autem labore reprehenderit veniam enim ipsam mollitia incidunt repellat necessitatibus, perferendis error quidem adipisci ea magni ad et repellendus neque. Quam mollitia unde dolorum atque tenetur dolor cupiditate, culpa ut officiis id voluptatum ipsum necessitatibus hic perspiciatis fugit repellendus voluptates. Labore ratione consequuntur sit hic officiis quibusdam voluptate aperiam quaerat qui ea accusamus, error recusandae assumenda minus doloribus reprehenderit animi minima vitae! Iure accusamus ab sint tempora distinctio obcaecati delectus, inventore sequi iusto perspiciatis quasi tenetur. Nisi facere est exercitationem dolorum, quis cupiditate eveniet nostrum facilis. Repudiandae cumque dicta corporis sit eligendi eaque sed dolorem molestias distinctio voluptatum iure, nobis incidunt aut! Eius nobis consectetur repellat! Eius, sapiente ipsam laboriosam fugiat quod ex quis, reiciendis, inventore facilis corporis repudiandae est quos sunt praesentium! Officia at quam obcaecati maiores reprehenderit perferendis, porro veniam? Reiciendis distinctio explicabo, cumque ex eum cum consectetur provident sint veniam labore, quibusdam eaque dicta tempora sequi magnam, temporibus porro. Debitis sapiente dolorem perspiciatis quam amet temporibus? Amet autem iure, sequi in debitis earum a, laudantium et facilis maxime voluptatum.</p>
+      <h2>Services Offered</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis rerum debitis unde nihil veniam iure enim nam illo sequi hic, laudantium culpa facilis esse in sapiente velit labore sint ipsam ea eveniet adipisci cupiditate. Explicabo eius reprehenderit vero quibusdam id magnam ullam veniam fugiat voluptate vel deserunt laudantium, quam voluptatibus!</p>
+      <p>We offer the following services</p>
+      <ul>
+        <li>Service 1</li>
+        <li>Service 2</li>
+        <li>Service 3</li>
+        <li>Service 4</li>
+        <li>Service 5</li>
+      </ul>
+      <h2>Contact us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit aspernatur adipisci omnis tempora aperiam facere labore voluptate quod facilis eveniet vitae, debitis accusantium quam magni sint necessitatibus similique? Dignissimos, eos.</p>
+    </div>
   )
 }
--- Tutorials/huntingcoder/pages/contact.js
+++ Tutorials/huntingcoder/pages/contact.js
@@ -1,7 +1,73 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styles from '../styles/Contact.module.css';
 
 export default function Contact() {
+  const [name, setname] = useState('')
+  const [email, setemail] = useState('')
+  const [phone, setphone] = useState('')
+  const [desc, setdesc] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(name, phone, email, desc);
+    const data = {name, email, phone, desc}
+    fetch('http://localhost:3000/api/postcontact/', {
+      method:'POST',
+      headers:{
+        'Content-type':'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then(response=>response.json())
+    .then(data=>{
+      console.log('Success:', data);
+      alert("Thanks for contacting us!")
+      setname('')
+      setemail('')
+      setphone('')
+      setdesc('')
+  })
+    .catch(error=>{console.log('Error:', error);})
+
+  }
+  
+  const handleChange = (e) => {
+    if(e.target.name=='name'){
+      setname(e.target.value)
+    }
+    if(e.target.name=='email'){
+      setemail(e.target.value)
+    }
+    if(e.target.name=='phone'){
+      setphone(e.target.value)
+    }
+    if(e.target.name=='desc'){
+      setdesc(e.target.value)
+    }
+    e.preventDefault();
+  }
+
   return (
-    <div>This is contact page.</div>
+    <div className={styles.container}>
+      <h1>Contact us</h1>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.mb3}>
+          <label htmlFor="name" className={styles.formLabel}>Name</label>
+          <input type="text" value={name} onChange={handleChange} name="name" className="form-control" id="name" aria-describedby="nameHelp"/>
+        </div>
+        <div className={styles.mb3}>
+          <label htmlFor="email" className={styles.formLabel}>Email Address</label>
+          <input type="email" value={email} onChange={handleChange} name="email" className="form-control" id="email" aria-describedby="emailHelp"/>
+        </div>
+        <div className={styles.mb3}>
+          <label htmlFor="phone" className={styles.formLabel}>Phone</label>
+          <input type="phone" value={phone} onChange={handleChange} className="form-control" name="phone" id="phone"/>
+        </div>
+        <div className={styles.mb3}>
+          <label htmlFor="desc" className={styles.formLabel}>Your concern</label>
+          <textarea className="form-control" value={desc} onChange={handleChange} name="desc" id="desc" placeholder='Write your text here' aria-describedby="descHelp"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
   )
 }
