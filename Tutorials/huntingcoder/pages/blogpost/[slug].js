f498faebe97b82652f01836f92f645594faf0652
Aaryan-R-S <aaryan20004@iiitd.ac.in>
2022-03-12 17:24:49 +0530

    Added till vid 31 Pushed HuntingCoder Blog

--- Tutorials/huntingcoder/pages/blogpost/[slug].js
+++ Tutorials/huntingcoder/pages/blogpost/[slug].js
@@ -27,12 +27,12 @@ const Slug = (props) => {
 }
 
 export async function getStaticPaths() {
+  let allBs = await fs.promises.readdir(`blogdata`)
+  allBs = allBs.map((item) => {
+    return { params: { slug: item.split(".")[0] } }
+  })
   return {
-    paths: [
-      { params: { slug: 'how-to-learn-flask' } },
-      { params: { slug: 'how-to-learn-javascript' } },
-      { params: { slug: 'how-to-learn-next-js' } },
-    ],
+    paths: allBs,
     fallback: true // fasle or blocking
   };
 }
